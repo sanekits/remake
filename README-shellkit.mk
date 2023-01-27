9a886733d78dd713d8d92cfe7d4fab5fd18060d9
Les Matheson <lmatheson4@bloomberg.net>
2023-01-27 05:18:14 -0500

    Build setup target progress

---
+++ README-shellkit.mk
@@ -0,0 +1,5 @@
+# README for shellkit integration
+
+Source `taskrc` and run `tmk` targets for build setup.
+
+Note that parts of the build setup use rsync to reach out and can't run behind a proxy.
--- shellkit-integration.mk
+++ shellkit-integration.mk
@@ -1,4 +1,4 @@
-# GNUmakefile for sanekits/remake (shellkit integration stuff, do not PR upstream)
+# shellkit-integration.mk for sanekits/remake
 
 DebianDepends := autotools-dev autoconf autopoint gcc pkg-config gettext libreadline-dev guile-2.2 make lzip
 
@@ -6,13 +6,18 @@ DebianDepends := autotools-dev autoconf autopoint gcc pkg-config gettext libread
 nope:
 	exit "Sorry, you need to read GNUmakefile"
 
+.PHONY: noproxy
+noproxy:
+	@# Fails if proxy is enabled
+	@curl -I artprod.dev.bloomberg.com && exit 1 || exit 0
+
 .PHONY: autotools-cfg
 autotools-cfg:
 	# For debian/ubuntu, -ish
 	sudo apt-get update && sudo apt-get install -y $(DebianDepends)
 
 .PHONY: build-setup
-build-setup:
+build-setup: noproxy
 	which autoconf || $(MAKE) autotools-cfg
 	./autogen.sh
 
